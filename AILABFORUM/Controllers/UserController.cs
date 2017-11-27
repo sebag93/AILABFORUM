1cfbc7e9cf629e383f8e49eaf99aed1f072f505f
Sebastian Guzdek <sebag93@gmail.com>
2017-11-27 21:35:44 +0100

    Utworzenie kontrolera użytkownika. Utworzenie modelu użytkownika. Utworzenie widoku rejestracji nowych użytkowników.

---
+++ AILABFORUM/Controllers/UserController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AILABFORUM.Models;
+
+namespace AILABFORUM.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registration
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        //Registration POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration(User user)
+        {
+            bool Status = false;
+            string message = "";
+            //walidacja modelu
+            if (ModelState.IsValid)
+            {
+
+                #region //email juz istnieje
+                var emailExist = IsEmailExist(user.email);
+                if (emailExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Podany email został już zarejestrowany");
+                    return View(user);
+                }
+                #endregion
+
+                #region //login juz istnieje
+                var loginExist = IsLoginExist(user.login);
+                if (loginExist)
+                {
+                    ModelState.AddModelError("LoginExist", "Podana nazwa użytkownika została już zarejestrowana");
+                    return View(user);
+                }
+                #endregion
+
+                #region //zapis do bazy danych
+                using (AILABFORUMEntities db = new AILABFORUMEntities())
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    message = "Rejestracja zakończona pomyślnie. Możesz zalogować sie na swoje konto.";
+                    Status = true;
+                }
+                #endregion
+            }
+            else
+            {
+                message = "Nieprawidłowe żądanie";
+            }
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(user);
+        }
+
+        //Czy email istnieje juz w bazie
+        [NonAction]
+        public bool IsEmailExist(string EMAIL)
+        {
+            using (AILABFORUMEntities db = new AILABFORUMEntities())
+            {
+                var v = db.Users.Where(x => x.email == EMAIL).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+        //Czy login istnieje juz w bazie
+        [NonAction]
+        public bool IsLoginExist(string LOGIN)
+        {
+            using (AILABFORUMEntities db = new AILABFORUMEntities())
+            {
+                var v = db.Users.Where(x => x.login == LOGIN).FirstOrDefault();
+                return v != null;
+            }
+        }
+    }
+}
---
+++ AILABFORUM/Models/Extended/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AILABFORUM.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+        public string powtorzhaslo { get; set; }
+    }
+
+    public class UserMetadata
+    {
+        [Display(Name = "Nazwa użytkownika")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Wprowadź nazwę użytkownika")]
+        public string login { get; set; }
+
+        [Display(Name="Imię")]
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Wprowadź imię")]
+        public string imie { get; set; }
+
+        [Display(Name ="Data urodzenia")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString ="{0:MM/dd/yyyy}")]
+        public DateTime data_urodzenia { get; set; }
+
+        [Display(Name ="Miejscowość")]
+        public string miejscowosc { get; set; }
+
+        [Display(Name ="Hasło")]
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Wprowadź hasło")]
+        [DataType(DataType.Password)]
+        [MinLength(6,ErrorMessage = "Hasło musi zawierać co najmniej 6 znaków")]
+        public string haslo { get; set; }
+
+        [Display(Name = "Powtórz hasło")]
+        [DataType(DataType.Password)]
+        [Compare("haslo",ErrorMessage ="Wprowadź poprawne hasło")]
+        public string powtorzhaslo { get; set; }
+
+        [Display(Name ="Adres email")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Wprowadź adres email")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Wprowadź prawidłowy adres email")]
+        public string email { get; set; }
+    }
+}
