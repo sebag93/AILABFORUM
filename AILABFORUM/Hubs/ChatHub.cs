8b1b77c76c4a2db640322ac6f9842984006b73c7
Sebastian Guzdek <sebag93@gmail.com>
2017-12-11 15:40:53 +0100

    CHATBOX wprowadzanie wiadomości tekstowych bez zapisywania do bazy danych

---
+++ AILABFORUM/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace AILABFORUM.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string date, string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(date, name, message);
+        }
+    }
+}
---
+++ AILABFORUM/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(AILABFORUM.Startup))]
+
+namespace AILABFORUM
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Aby uzyskać więcej informacji o sposobie konfigurowania aplikacji, odwiedź stronę https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
